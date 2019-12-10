8babc505f3540341f1d9a365d6c73803184728ee
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2019-12-10 15:08:38 +0100

    ES6 syntax

--- RainbowColor.js
+++ RainbowColor.js
@@ -1,63 +1,79 @@
-// import { exportSpecifier } from "babel-types"
+const { sin, cos, PI, ceil, floor, random } = Math;
 
-exports.bojaRND = function() {
-	let hex = `000000` + Math.floor(Math.random() * 0xffffff).toString(16)
-	return `#` + hex.slice(-6)
-}
-exports.bojaHslaH = function(range, red) {
-	let h = (360 / range) * red
-	let s = Math.floor(Math.random() * 100)
-	let l = Math.floor(Math.random() * 100)
-	let a = Math.random()
-	return `hsla(${h}, ${s}%, ${l}%, ${a})`
-}
-exports.bojaHslaS = function(range, red) {
-	let h = Math.floor(Math.random() * 360)
-	let s = (100 / range) * red
-	let l = Math.floor(Math.random() * 100)
-	let a = Math.random()
-	return `hsla(${h}, ${s}%, ${l}%, ${a})`
-}
-exports.bojaHslaA = function(range, red) {
-	let h = Math.floor(Math.random() * 360)
-	let s = Math.floor(Math.random() * 100)
-	let l = Math.floor(Math.random() * 100)
-	let a = (1 / range) * red
-	return `hsla(${h}, ${s}%, ${l}%, ${a})`
-}
-exports.bojaHsla = function(range, red) {
-	let h = (360 / range) * red
-	let s = (100 / range) * red
-	let l = 60
-	let a = (1 / range) * red
-	return `hsla(${h}, ${s}%, ${l}%, ${a})`
-}
+exports.bojaRND = () => {
+	let hex = `000000` + floor(random() * 0xffffff).toString(16);
+	return `#` + hex.slice(-6);
+};
 
-exports.bojaHEX = function(range, i) {
-	let frequency = 2*Math.PI/range
-	let r = Math.ceil(Math.sin(frequency * i + 0) * 127 + 128)
-	let g = Math.ceil(Math.sin(frequency * i + 2) * 127 + 128)
-	let b = Math.ceil(Math.sin(frequency * i + 4) * 127 + 128)
-	let b1 = new boja(r,g,b)
-	return b1.hex
-}
+exports.bojaHslaH = (range, red) => {
+	let h = (360 / range) * red;
+	let s = floor(random() * 100);
+	let l = floor(random() * 100);
+	let a = random();
+	return `hsla(${h}, ${s}%, ${l}%, ${a})`;
+};
+
+exports.bojaHslaS = (range, red) => {
+	let h = floor(random() * 360);
+	let s = (100 / range) * red;
+	let l = floor(random() * 100);
+	let a = random();
+	return `hsla(${h}, ${s}%, ${l}%, ${a})`;
+};
+
+exports.bojaHslaA = (range, red) => {
+	let h = floor(random() * 360);
+	let s = floor(random() * 100);
+	let l = floor(random() * 100);
+	let a = (1 / range) * red;
+	return `hsla(${h}, ${s}%, ${l}%, ${a})`;
+};
+
+exports.bojaHsla = (range, red) => {
+	let h = (360 / range) * red;
+	let s = (100 / range) * red;
+	let l = 60;
+	let a = (1 / range) * red;
+	return `hsla(${h}, ${s}%, ${l}%, ${a})`;
+};
+
+exports.rainbowColor = (range, i) => {
+	let r = fetchComponent(range, i);
+	let g = fetchComponent(range, i);
+	let b = fetchComponent(range, i);
+
+	return new boja(r, g, b).hex;
+};
 
 class boja {
-	constructor(r,g,b) {
-		this.r = r,
-		this.g = g,
-		this.b = b
+	constructor(r, g, b) {
+		(this.r = r), (this.g = g), (this.b = b);
 	}
-	get hex(){
-		// return `#${this.r.toString(16)}${this.g.toString(16)}${this.b.toString(16)}`
-		return `#` + byte2Hex(this.r) + byte2Hex(this.g) + byte2Hex(this.b)
+	get hex() {
+		return `#` + byte2Hex(this.r) + byte2Hex(this.g) + byte2Hex(this.b);
 	}
-	get rgb(){
-		return `rgb(${this.r.toString(10)},${this.g.toString(10)},${this.b.toString(10)})`
+	get rgb() {
+		return `rgb(${this.r.toString(10)},${this.g.toString(10)},${this.b.toString(
+			10
+		)})`;
 	}
 }
 
-function byte2Hex(n) {
-	var nybHexString = `0123456789ABCDEF`
-	return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1)
-}
+const byte2Hex = byte => {
+	const nybHexString = `0123456789ABCDEF`;
+	return (
+		String(nybHexString.substr((byte >> 4) & 0x0f, 1)) +
+		nybHexString.substr(byte & 0x0f, 1)
+	);
+};
+
+const fetchComponent = (range, i, comp) =>
+	ceil(sin(((2 * PI) / range) * i + 0) * 127 + 128);
+
+// testing
+console.log(this.rainbowColor(100, 4));
+console.log(this.rainbowColor(100, 5));
+console.log(this.rainbowColor(100, 6));
+console.log(this.rainbowColor(100, 7));
+console.log(this.rainbowColor(100, 466));
+console.log(this.rainbowColor(100, 66));
